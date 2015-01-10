32728b347362a75b612d7ffd308597497238ea3b
zirkonit <zirkonit@gmail.com>
2015-01-11 02:36:15 +0300

    Testing progress

--- test/dformat/core_test.clj
+++ test/dformat/core_test.clj
@@ -5,6 +5,12 @@
 
 (deftest stamp-test
 	(let [date (t/local-date 2011 6 9)] 
-		  (testing "FIXME, I fail."
-			(is (= (dformat date "March 1, 1999") "June 9, 2011"))))
+		  (testing "Stamp code examples"
+			(is (= (dformat date "March 1, 1999") "June 9, 2011"))
+			(is (= (dformat date "Jan 1, 1999") "Jun 9, 2011"))
+			(is (= (dformat date "Jan 01") "Jun 09"))
+			(is (= (dformat date "Sunday, May 1, 2000") "Thursday, June 9, 2011"))
+			(is (= (dformat date "Sun Aug 5") "Jun 9, 2011"))
+			(is (= (dformat date "12/31/99") "06/09/11"))
+			(is (= (dformat date "DOB: 12/31/2000") "DOB: 06/09/2011"))))
 	)

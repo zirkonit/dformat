1f5d778b275f0cae71dcd89522772415c2aaa531
zirkonit <zirkonit@gmail.com>
2015-01-10 18:31:54 +0300

    Prime failing test

--- test/dformat/core_test.clj
+++ test/dformat/core_test.clj
@@ -1,7 +1,10 @@
 (ns dformat.core-test
   (:require [clojure.test :refer :all]
-            [dformat.core :refer :all]))
+            [dformat.core :refer :all]
+            [clj-time.core :as t]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest stamp-test
+	(let [date (t/local-date 2011 6 9)] 
+		  (testing "FIXME, I fail."
+			(is (= (dformat date "March 1, 1999") "June 9, 2011"))))
+	)

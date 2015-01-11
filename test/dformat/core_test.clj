137ffcd84e41ee51da158b9a98ba8d255e14451c
zirkonit <zirkonit@gmail.com>
2015-01-11 23:54:28 +0300

    Minor fixes in test suite

--- test/dformat/core_test.clj
+++ test/dformat/core_test.clj
@@ -4,13 +4,14 @@
             [clj-time.core :as t]))
 
 (deftest stamp-test
-	(let [date (t/local-date 2011 6 9)] 
+	(let [date (t/date-time 2011 6 9)] 
 		  (testing "Stamp code examples"
 			(is (= (dformat date "March 1, 1999") "June 9, 2011"))
 			(is (= (dformat date "Jan 1, 1999") "Jun 9, 2011"))
 			(is (= (dformat date "Jan 01") "Jun 09"))
 			(is (= (dformat date "Sunday, May 1, 2000") "Thursday, June 9, 2011"))
-			(is (= (dformat date "Sun Aug 5") "Jun 9, 2011"))
+			(is (= (dformat date "Sun Aug 5") "Thu Jun 9"))
 			(is (= (dformat date "12/31/99") "06/09/11"))
-			(is (= (dformat date "DOB: 12/31/2000") "DOB: 06/09/2011"))))
+			(is (= (dformat date "DOB: 12/31/2000") "DOB: 06/09/2011"))
+			(is (= (dformat date "March 15, 1999") "June 09, 2011"))))
 	)
